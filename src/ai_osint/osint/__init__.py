e3bf8f296eaf892933184af1c11eefb98fb7c94c
purplelemons-dev <ironbreaker2005@gmail.com>
2024-04-03 20:40:10 -0500

    Update getAllSourceInfo function parameters with sources

--- src/ai_osint/osint/__init__.py
+++ src/ai_osint/osint/__init__.py
@@ -1,10 +1,12 @@
 def getAllSourceInfo(
-    name: str = None,
-    phone: str = None,
-    email: str = None,
-    username: str = None,
-    address: str = None,
-    ip: str = None,
-    domain: str = None,
+    name: str = None, # dehashed, tps, checkleaked
+    phone: str = None, # dehashed, tps
+    email: str = None, # dehashed, tps, checkleaked
+    username: str = None, # dehashed, checkleaked
+    address: str = None, # dehashed, tps
+    ip: str = None, # ip2location, dehashed, checkleaked
+    domain: str = None, 
+    password: str = None, # dehashed, checkleaked
+    hashed_password: str = None, # dehashed, checkleaked, md5hashing.net
 ):
     pass
